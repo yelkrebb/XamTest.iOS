51e8d6349bea285037f4d32dc0544b4e391bc37d
Berkley Bernales <Berkley@Berks-MacBook-Pro.local>
2016-07-22 17:39:41 +0800

    Initial commit

---
+++ AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using XamTest;
+
+using Foundation;
+using UIKit;
+
+namespace XamTest.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+			App.SetAdapter (Motion.Mobile.Core.BLE.Adapter.Current);
+
+			LoadApplication (new App ());
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# XamTest.iOS
